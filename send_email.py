96827f326848612731375e8610f36c2dab6377f9
MARTRENCHAR <paul.martrenchar@idemia.com>
2025-01-08 09:59:07 +0100

    switch back to gmail

--- send_email.py
+++ send_email.py
@@ -5,9 +5,8 @@ from email.mime.multipart import MIMEMultipart
 
 def send_mail(content):
     sender_email = "bot.martrenchar@gmail.com"
-    identifier = "831031001@smtp-brevo.com"
     receiver_email = "paul.martrenchar@gmail.com"
-    password = os.environ.get('BREVO')
+    password = os.environ.get('GMAIL_PASSWORD')
 
     message = MIMEMultipart("related")
     message["Subject"] = "Job offers"
@@ -17,12 +16,12 @@ def send_mail(content):
     message.attach(MIMEText(content, "html"))
 
     # Create secure connection with server and send email
-    with smtplib.SMTP('smtp-relay.brevo.com', 587) as server:
+    with smtplib.SMTP('smtp.gmail.com', 587) as server:
         # Enable security
         server.starttls()
         
         # Login to the server
-        server.login(identifier, password)
+        server.login(sender_email, password)
         
         # Send email
         server.sendmail(
