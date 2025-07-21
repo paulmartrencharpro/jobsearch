f5b0eac92e183a220430ff56a92db0d7a8d48d6c
Paul Martrenchar <paul.martrenchar@idemia.com>
2025-07-21 11:25:51 +0200

    Reduce timings on Mistral

--- AI/mistral.py
+++ AI/mistral.py
@@ -14,20 +14,20 @@ def call_ai(prompt, json_mode):
     except SDKError as e:
         #Wait, then try again once
         print(f"failed once - {e.message}")
-        sleep(10)
+        sleep(5)
         try:
             return _call_ai(prompt, json_mode)
         except SDKError as e:
             #Wait, then try again once
             print(f"failed twice - {e.message}")
-            sleep(65)
+            sleep(10)
             return _call_ai(prompt, json_mode)
     except Exception as e:
         # Throw the error if it's not an SDKError
         raise
 
 def _call_ai(prompt, json_mode):
-    sleep(1.1)
+    sleep(0.1)
     client = Mistral(api_key=os.getenv('MISTRAL_KEY'))
 
     extra_param = {}
